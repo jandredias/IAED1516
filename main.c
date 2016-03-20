feb7fcf95cc6b1bef1352aff756d9c4497fa74e6
Miguel Leitao Belem <mbelem@sysmail.me>
2016-03-20 13:04:29 +0000

    nada dee novo

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define MAX_AIRPORT 1000
 #include <limits.h>
 
+#define DEBUG 0
 /****************************************************************************
  *
  * STRUCTS
@@ -26,12 +27,13 @@ int matriz[MAX_AIRPORT][MAX_AIRPORT];
 int contador_aeroportos = 0;
 /****************************************************************************
 *
-HEADER
+*  HEADER
 * 
 *****************************************************************************/
 void init();
 int strlen2(char c[]);
 int adiciona_aeroportos();
+int altera_capacidade_maxima();
 
 /****************************************************************************
 *
@@ -39,69 +41,77 @@ int adiciona_aeroportos();
 *
 ****************************************************************************/
 int main() {
-    char command;
-    int result;
-    init();
-    do {
-        command = getchar();
-        switch (command) {
-        case 'A':
-            result = adiciona_aeroportos();
-            if (result != 0) {
-                if (result == 1) {
-                    printf("There was an error: invalid Id.\n");
-                }
-                if (result == 2) {
-                    printf("There was an error: Invalid capacity\n");
-                    exit(1);
-                }
-            }
-            break;
-       /* case 'I':
-            adiciona_airoportos();
-            break;
-        case 'F':
-            adiciona_airoportos();
-            break;
-        case 'G':
-            adiciona_airoportos();
-            break;
-        case 'R':
-            adiciona_airoportos();
-            break;
-        case 'S':
-            adiciona_airoportos();
-            break;
-        case 'N':
-            adiciona_airoportos();
-            break;
-        case 'P':
-            adiciona_airoportos();
-            break;
-        case 'Q':
-            adiciona_airoportos();
-            break;
-        case 'V':
-            adiciona_airoportos();
-            break;
-        case 'C':
-            adiciona_airoportos();
-            break;
-        case 'O':
-            adiciona_airoportos();
-            break;
-        case 'L':
-            adiciona_airoportos();
-            break;
-        case 'X':
-            adiciona_airoportos();
-            break;
-        default:
-            puts("Command not found");*/
-        }
-    }
-    while (command != 'X');
-    return 0;
+	char command;
+	int result;
+	init();
+	do {
+		#if DEBUG
+		printf("Estou a ler o comando\n");
+		#endif
+		command = getchar();
+	
+		#if DEBUG
+		printf("Já li comando: %c\n",command);
+		#endif
+		
+		switch (command) {
+		case 'A':
+			result = adiciona_aeroportos();
+			if (result != 0) {
+				if (result == 1) {
+					printf("There was an error: invalid Id.\n");
+				}
+				if (result == 2) {
+					printf("There was an error: Invalid capacity\n");
+					exit(1);
+				}
+			}
+			break;
+	        case 'I':
+			altera_capacidade_maxima();
+			break;
+		case 'F':
+			adiciona_airoportos();
+			break;
+		case 'G':
+			adiciona_airoportos();
+			break;
+		case 'R':
+			adiciona_airoportos();
+			break;
+		case 'S':
+			adiciona_airoportos();
+			break;
+		case 'N':
+			adiciona_airoportos();
+			break;
+		case 'P':
+			adiciona_airoportos();
+			break;
+		case 'Q':
+			adiciona_airoportos();
+			break;
+		case 'V':
+			adiciona_airoportos();
+			break;
+		case 'C':
+			adiciona_airoportos();
+			break;
+		case 'O':
+			adiciona_airoportos();
+			break;
+		case 'L':
+			adiciona_airoportos();
+			break;
+		case 'X':
+			adiciona_airoportos();
+			break;
+		default:
+			puts("Command not found");*/
+		}
+	}
+	while (command != 'X');
+	return 0;
 }
 
 /*****************************************************************************
@@ -124,11 +134,11 @@ void init(){
 	}
 }
 int strlen2(char c[]){
-          int index = 0;
-          while(c[index] != '\0'){
-                  index++;
-    }
-          return index; /*meti isto pq n sei ate que ponto podes usar a biblioteca da linha1 */
+		  int index = 0;
+		  while(c[index] != '\0'){
+				  index++;
+	}
+		  return index; /*meti isto pq n sei ate que ponto podes usar a biblioteca da linha1 */
 }
 
 /***************************************************************************
@@ -141,23 +151,31 @@ int strlen2(char c[]){
  * Incrementa tambem o contador global de aeroportos
  *
  * return: 0 on success
- *         1 on failure invalid id
- *         2 on failure if capacity is bigger than max
- *           unsigned int or less then zero
+ *		 1 on failure invalid id
+ *		 2 on failure if capacity is bigger than max
+ *		   unsigned int or less then zero
  *
  */
 int adiciona_aeroportos() {
 	scanf("%s", airport[contador_aeroportos].id);
 	scanf("%u", &airport[contador_aeroportos].capacity);
-	contador_aeroportos++; 
 	if(strlen2(airport[contador_aeroportos].id) != 3)
 		return 1; 
-  else if(airport[contador_aeroportos].capacity <= 0 ||
-          airport[contador_aeroportos].capacity > UINT_MAX)
-		return 2; 
-	else
+  	else if(airport[contador_aeroportos].capacity <= 0 ||
+		airport[contador_aeroportos].capacity > UINT_MAX)
+		return 2;
+
+	contador_aeroportos++;
 		return 0;
 }
-
+int altera_capacidade_maxima() {
+	char i[3];
+	int d = 0, k = 0, nova
+	for(k; k < 3; k++){
+		i[k] = getchar();
+	if((d = getchar()) != '\0' || d != '\t' || d != '\n')
+		/*criar o erro*/
+	
+	
 
  
